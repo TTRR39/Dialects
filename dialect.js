3363ecaddc115b3fd9cbfbe0425b4baf5004f3a8
TTRR39 <chengdutian33@gmail.com>
2026-08-12 15:25:10 +0100

    Update dialect.js

--- dialect.js
+++ dialect.js
@@ -1,1 +1,19 @@
+const dialectData = [
+  {
+    word: "食",
+    pronunciation: "sik˧˥",
+    speaker: "Speaker 01",
+    exampleDialect: "我食咗饭。",
+    exampleMandarin: "我吃过饭。",
+    mandarin: "吃"
+  },
 
+  {
+    word: "饮",
+    pronunciation: "jam˧˥",
+    speaker: "Speaker 01",
+    exampleDialect: "我饮水。",
+    exampleMandarin: "我喝水。",
+    mandarin: "喝"
+  }
+];
