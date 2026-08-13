27a8bad95e1088dabdc84018b2659aaaaf55c39d
TTRR39 <chengdutian33@gmail.com>
2026-08-13 11:39:25 +0100

    Add new temporal terms with examples and pronunciations

--- data.js
+++ data.js
@@ -21,13 +21,63 @@ const dialectData = [
     wordsID: "002"
   },
     {
-    word: "",
-    pronunciation: "",
+    word: "赶明儿",
+    pronunciation: "/kan35 miər22/",
     speaker: "Speaker 01",
-    exampleDialect: "。",
-    exampleMandarin: "。",
-    mandarin: "昨天",
+    exampleDialect: "俺们赶明儿赶集去呗。",
+    exampleMandarin: "我们明天去赶集吧。",
+    mandarin: "明天",
      category:"时间词（temporal terms）",
-    wordsID: "002"
+    wordsID: "003"
+  },
+  {
+    word: "前儿个",
+    pronunciation: "/tshiɐr22 kə0/",
+    speaker: "Speaker 01",
+    exampleDialect: "我前儿个往家待着=拧。",
+    exampleMandarin: "我前天在家待着呢。",
+    mandarin: "前天",
+     category:"时间词（temporal terms）",
+    wordsID: "004"
+  },
+  {
+    word: "过了明儿",
+    pronunciation: "/kuo55 lə0 miər22/",
+    speaker: "Speaker 01",
+    exampleDialect: "过了明儿再去。",
+    exampleMandarin: "后天再去。",
+    mandarin: "后天",
+     category:"时间词（temporal terms）",
+    wordsID: "005"
+  },
+  {
+    word: "黑家",
+    pronunciation: "/xei51 tɕiɛ0/",
+    speaker: "Speaker 01",
+    exampleDialect: "黑家吃么也。",
+    exampleMandarin: "晚上吃什么呀。",
+    mandarin: "晚上",
+     category:"时间词（temporal terms）",
+    wordsID: "006"
+  },
+  {
+    word: "早清",
+    pronunciation: "/tsau21 tshiŋ45/",
+    speaker: "Speaker 01",
+    exampleDialect: "早清才家去。",
+    exampleMandarin: "早上才回家。",
+    mandarin: "早上",
+     category:"时间词（temporal terms）",
+    wordsID: "007"
+  },
+  {
+    word: "大早清",
+    pronunciation: "/ta24 tsau21 tshiŋ45/",
+    speaker: "Speaker 01",
+    exampleDialect: "今儿个大早清下雾咧。",
+    exampleMandarin: "今天清晨起雾了。",
+    mandarin: "清晨",
+     category:"时间词（temporal terms）",
+    wordsID: "008"
   }
 ];
